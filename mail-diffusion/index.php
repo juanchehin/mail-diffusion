7f217c3d51a41b7aa0a531a69195fa5dd08c5b5c
chehin <chehinjuan@gmail.com>
2021-12-25 08:39:50 -0300

    formulario de mail (input) + formato de mail obligatorio

--- mail-diffusion/index.php
+++ mail-diffusion/index.php
@@ -457,23 +457,25 @@
 		<!-- MODAL -->
 		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 			<div class="modal-dialog">
-				<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title">Subcribete</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
-				<div class="modal-body">
-				<div class="input-group mb-3">
-					<span class="input-group-text" id="basic-addon1">@</span>
-					<input type="text" class="form-control" placeholder="Correo electronico" aria-label="Username" aria-describedby="basic-addon1">
+				<form method="post" id="usuarios_form">
+					<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title">Subcribete</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adip </p>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Registrarse</button>
-					<button type="button" class="btn btn-primary">Cerrar</button>
-				</div>
-				</div>
+					<div class="modal-body">
+					<div class="input-group mb-3">
+						<span class="input-group-text" id="basic-addon1">@</span>
+							<input type="email" class="form-control" placeholder="Correo electronico" aria-label="Username" aria-describedby="basic-addon1" required>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adip </p>
+					</div>
+					<div class="modal-footer">
+						<button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Registrarse</button>
+						<button type="button" class="btn btn-primary">Cerrar</button>
+					</div>
+					</div>
+				</form>
 			</div>
 		</div>
 
